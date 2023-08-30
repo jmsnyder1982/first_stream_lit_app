2e2c115a78ace0fd5f0152e369227420f5094961
jmsnyder1982 <58143459+jmsnyder1982@users.noreply.github.com>
2023-08-30 17:18:06 -0400

    Update streamlit_app.py
    - updated emoji in smoothie header from orange to mango

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,4 +9,4 @@ streamlit.text('🥗 Kale, Spinach, & Rocket Smootie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑 🍞 Avocado Toast')
 
-streamlit.header('🍌🍊 Build Your Own Fruit Smoothie 🥝🍇')
+streamlit.header('🍌🥭Build Your Own Fruit Smoothie 🥝🍇')
