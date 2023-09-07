5e9765e457d61937321f43ad33959cc24fd231ce
jmsnyder1982 <58143459+jmsnyder1982@users.noreply.github.com>
2023-09-07 10:52:16 -0400

    Update streamlit_app.py
    updated add my fruit section

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,5 +47,5 @@ streamlit.dataframe(my_data_row)
 add_my_fruit = streamlit.text_input('What fruit would you like to add?')
 #get status response of add my fruit call
 add_my_fruit_response = requests.get('https://fruityvice.com/api/fruit/' + add_my_fruit)
-streamlit.write('Thanks for adding', add_my_fruit_response)
+streamlit.write('Thanks for adding' + add_my_fruit)
 
