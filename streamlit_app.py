72f69621c0b25fa5d793335d1321360a2f5b9f82
jmsnyder1982 <58143459+jmsnyder1982@users.noreply.github.com>
2023-09-07 11:05:49 -0400

    Update streamlit_app.py
    adding controls to stop the app at a certain line while I am troubleshooting the control issues

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,6 +37,9 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 #displays the normalized version in a dataframe
 streamlit.dataframe(fruityvice_normalized)
 
+#don't run anything past here while we troubleshoot
+streamlit.stop()
+
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT * from fruit_load_list")
