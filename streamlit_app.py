0f954dabc16c58c3382393232ff0228a9ac0d0a4
jmsnyder1982 <58143459+jmsnyder1982@users.noreply.github.com>
2023-09-07 11:18:40 -0400

    Update streamlit_app.py
    added some structure - using a try/except block

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,16 +27,25 @@ streamlit.dataframe(fruits_to_show)
 
 #New Section to display Fruityvice API response
 streamlit.header('Fruityvice Fruit Advice')
-fruit_choice = streamlit.text_input('What fruit would you like info about?', 'Kiwi')
-#get status response of fruityvice call
-fruityvice_response = requests.get('https://fruityvice.com/api/fruit/' + fruit_choice)
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like info about?', 'Kiwi')
+  if not fruit_choice
+    streamlit.error("Please select a fruit to get information.")
+  else:
+    #get status response of fruityvice call
+    fruityvice_response = requests.get('https://fruityvice.com/api/fruit/' + fruit_choice)
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    #displays the normalized version in a dataframe
+    streamlit.dataframe(fruityvice_normalized)
+
+except URLError as e:
+  stremlit.error()
+  
+
 streamlit.write('The user entered', fruit_choice)
-
 #takes the json version of the response and normalizes it
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 
-#displays the normalized version in a dataframe
-streamlit.dataframe(fruityvice_normalized)
+
 
 #don't run anything past here while we troubleshoot
 streamlit.stop()
